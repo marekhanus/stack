89320ac7dd0bc977f0d74f21eb6066a0263ea6d1
Marek Hanuš <marek.hanus.st@vsb.cz>
2018-12-28 00:12:01 +0100

    Argument 'getClass().getClassLoader().getResource("sample/sample.fxml")' might be null

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,12 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample/sample.fxml"));
+        Parent root = FXMLLoader.load(
+                Objects.requireNonNull(getClass().getClassLoader().getResource("sample/sample.fxml"))
+        );
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
