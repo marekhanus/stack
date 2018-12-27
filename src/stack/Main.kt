9070384818c8686d35f65732bbabbdc173fe1996
Marek Hanuš <marek.hanus.st@vsb.cz>
2018-12-28 00:36:06 +0100

    Main class: Java to Kotlin

---
+++ src/stack/Main.kt
@@ -0,0 +1,29 @@
+package stack
+
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
+import java.net.URL
+import java.util.Objects
+
+class Main : Application() {
+
+    @Throws(Exception::class)
+    override fun start(primaryStage: Stage) {
+        val root = FXMLLoader.load<Parent>(
+            Objects.requireNonNull<URL>(javaClass.classLoader.getResource("stack/view.fxml"))
+        )
+        primaryStage.title = "Stack"
+        primaryStage.scene = Scene(root, (1080 / 2).toDouble(), (1920 / 2).toDouble())
+        primaryStage.show()
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Application.launch(*args)
+        }
+    }
+}
