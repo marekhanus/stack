fb82fde7f59949e1476af3e49045c695c052fac2
Marek Hanuš <marek.hanus.st@vsb.cz>
2018-12-30 19:26:13 +0100

    StackMoveControl optimized

--- src/main/java/stack/control/StackMoveControl.kt
+++ src/main/java/stack/control/StackMoveControl.kt
@@ -6,7 +6,10 @@ import com.almasb.fxgl.entity.Entity
 import com.almasb.fxgl.entity.component.PositionComponent
 import stack.StackBlock
 
-class StackMoveControl : Control() {
+class StackMoveControl(
+        private val minPosition: Double = - StackBlock.width / 2.0,
+        private val maxPosition: Double = FXGL.getApp().width - StackBlock.width / 2.0
+) : Control() {
 
     private val position: PositionComponent = PositionComponent()
     private var direction: Boolean = true
@@ -15,12 +18,9 @@ class StackMoveControl : Control() {
         position.x += 1.0 * (if (direction) 1 else -1)
         position.y += 1.0 * (if (direction) 1 else -1)
 
-        val min = - StackBlock.width / 2.0
-        val max = FXGL.getApp().width - StackBlock.width / 2.0
-
         if (
-                position.x == min || position.y == min ||
-                position.x == max || position.y == max
+                position.x == minPosition || position.y == minPosition ||
+                position.x == maxPosition || position.y == maxPosition
         ) {
             direction = !direction
         }
