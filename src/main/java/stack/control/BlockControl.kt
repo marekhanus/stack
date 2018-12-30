0af738e01919d41078d0bb956f55c01c0b455847
Marek Hanuš <marek.hanus.st@vsb.cz>
2018-12-30 15:23:03 +0100

    BlockControl class

---
+++ src/main/java/stack/control/BlockControl.kt
@@ -0,0 +1,11 @@
+package stack.control
+
+import com.almasb.fxgl.entity.Control
+import com.almasb.fxgl.entity.Entity
+
+class BlockControl : Control() {
+
+    override fun onUpdate(p0: Entity?, p1: Double) {
+        TODO("not implemented")
+    }
+}
