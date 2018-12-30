82f6b2151f231ea5d4fa96ab47c1ccc1c677f825
Marek Hanuš <marek.hanus.st@vsb.cz>
2018-12-30 17:24:57 +0100

    StackBlock generating by polygon

--- src/main/java/stack/StackBlock.kt
+++ src/main/java/stack/StackBlock.kt
@@ -1,18 +1,29 @@
 package stack
 
 import com.almasb.fxgl.entity.view.EntityView
-import javafx.scene.Node
 import javafx.scene.paint.Color
-import javafx.scene.shape.Rectangle
+import javafx.scene.shape.Polygon
 
 class StackBlock(private val view: EntityView = EntityView(), val size: Int = 50) {
 
-    fun get(): Node? {
+    fun get(): EntityView {
         return this.view
     }
 
     init {
-        view.addNode(Rectangle(20.0, 10.0, Color.BLUE))
-        view.addNode(Rectangle(10.0, 20.0, Color.RED))
+        view.addNode(this.getBaseBlock())
+    }
+
+    private fun getBaseBlock(): Polygon {
+        val block = Polygon(
+                10.0, 10.0,
+                10.0, 20.0,
+                20.0, 20.0,
+                20.0, 10.0
+        )
+
+        block.fill = Color.GRAY
+
+        return block
     }
 }
