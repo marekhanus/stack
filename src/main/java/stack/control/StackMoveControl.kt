eed21d7ce0d7c24300dee6a9803af56f8659b35d
Marek Hanuš <marek.hanus.st@vsb.cz>
2018-12-30 20:05:48 +0100

    StackMoveControl direction updated

--- src/main/java/stack/control/StackMoveControl.kt
+++ src/main/java/stack/control/StackMoveControl.kt
@@ -5,6 +5,7 @@ import com.almasb.fxgl.entity.Control
 import com.almasb.fxgl.entity.Entity
 import com.almasb.fxgl.entity.component.PositionComponent
 import stack.StackBlock
+import stack.StackDirection
 
 class StackMoveControl(
         private val minPosition: Double = - StackBlock.width / 2.0,
@@ -12,17 +13,17 @@ class StackMoveControl(
 ) : Control() {
 
     private val position: PositionComponent = PositionComponent()
-    private var direction: Boolean = true
+    private var direction: StackDirection = StackDirection.LEFT
 
     override fun onUpdate(p0: Entity?, p1: Double) {
-        position.x += 1.0 * (if (direction) 1 else -1)
-        position.y += 1.0 * (if (direction) 1 else -1)
+        position.x += 1.0 * (if (direction == StackDirection.LEFT) 1 else -1)
+        position.y += 1.0 * (if (direction == StackDirection.LEFT) 1 else -1)
 
         if (
                 position.x == minPosition || position.y == minPosition ||
                 position.x == maxPosition || position.y == maxPosition
         ) {
-            direction = !direction
+            direction = if (direction == StackDirection.LEFT) StackDirection.RIGHT else StackDirection.LEFT
         }
     }
 }
