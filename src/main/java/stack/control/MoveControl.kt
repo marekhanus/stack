ee793b9dcfa62cbc89169093a6a62b5a42afabe8
Marek Hanuš <marek.hanus.st@vsb.cz>
2018-12-30 19:19:49 +0100

    MoveControl moving updated

--- src/main/java/stack/control/MoveControl.kt
+++ src/main/java/stack/control/MoveControl.kt
@@ -1,15 +1,28 @@
 package stack.control
 
+import com.almasb.fxgl.app.FXGL
 import com.almasb.fxgl.entity.Control
 import com.almasb.fxgl.entity.Entity
 import com.almasb.fxgl.entity.component.PositionComponent
+import stack.StackBlock
 
 class MoveControl : Control() {
 
-    private val position: PositionComponent = PositionComponent(0.0, 0.0)
+    private val position: PositionComponent = PositionComponent()
+    private var direction: Boolean = true
 
     override fun onUpdate(p0: Entity?, p1: Double) {
-        position.x += 1.0
-        position.y += 0.0
+        position.x += 1.0 * (if (direction) 1 else -1)
+        position.y += 1.0 * (if (direction) 1 else -1)
+
+        val min = - StackBlock.width / 2.0
+        val max = FXGL.getApp().width - StackBlock.width / 2.0
+
+        if (
+                position.x == min || position.y == min ||
+                position.x == max || position.y == max
+        ) {
+            direction = !direction
+        }
     }
 }
