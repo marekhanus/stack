6a7461c8e24f1739add78dd4f68506cc38fb692e
Marek Hanuš <marek.hanus.st@vsb.cz>
2018-12-30 17:52:05 +0100

    StackBlock width, height, private vals

--- src/main/java/stack/StackBlock.kt
+++ src/main/java/stack/StackBlock.kt
@@ -4,7 +4,12 @@ import com.almasb.fxgl.entity.view.EntityView
 import javafx.scene.paint.Color
 import javafx.scene.shape.Polygon
 
-class StackBlock(private val view: EntityView = EntityView(), val size: Double = 50.0, val height: Double = 5.0) {
+class StackBlock(
+    private val view: EntityView = EntityView(),
+    private val width: Double = 50.0,
+    private val height: Double = 35.0,
+    private val thickness: Double = 5.0
+) {
 
     fun get(): EntityView {
         return this.view
@@ -18,10 +23,10 @@ class StackBlock(private val view: EntityView = EntityView(), val size: Double =
 
     private fun getBaseBlock(): Polygon {
         val block = Polygon(
-                0.0, size / 2.0, // left corner
-                size / 2.0, 0.0, // top corner
-                size, size / 2.0, // right corner
-                size / 2.0, size // bottom corner
+                0.0, height / 2.0, // left corner
+                width / 2.0, 0.0, // top corner
+                width, height / 2.0, // right corner
+                width / 2.0, height // bottom corner
         )
 
         block.fill = Color.GRAY
@@ -31,10 +36,10 @@ class StackBlock(private val view: EntityView = EntityView(), val size: Double =
 
     private fun getBottomLeftBlock(): Polygon {
         val block = Polygon(
-                0.0, size / 2.0, // top left corner
-                size / 2.0, size, // top right corner
-                size / 2.0, size + height, // bottom right corner
-                0.0, size / 2.0 + height // bottom left corner
+                0.0, height / 2.0, // top left corner
+                width / 2.0, height, // top right corner
+                width / 2.0, height + thickness, // bottom right corner
+                0.0, height / 2.0 + thickness // bottom left corner
         )
 
         block.fill = Color.DARKSLATEGRAY
@@ -44,10 +49,10 @@ class StackBlock(private val view: EntityView = EntityView(), val size: Double =
 
     private fun getBottomRightBlock(): Polygon {
         val block = Polygon(
-                size / 2.0, size, // top left corner
-                size, size / 2.0, // top right corner
-                size, size / 2.0 + height, // bottom right corner
-                size / 2.0, size + height // bottom left corner
+                width / 2.0, height, // top left corner
+                width, height / 2.0, // top right corner
+                width, height / 2.0 + thickness, // bottom right corner
+                width / 2.0, height + thickness // bottom left corner
         )
 
         block.fill = Color.DARKGRAY
