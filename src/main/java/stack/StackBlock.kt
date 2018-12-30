ffedaec38efaf3244bf04af8a649ca0ce81cc695
Marek Hanuš <marek.hanus.st@vsb.cz>
2018-12-30 17:17:21 +0100

    StackBlock class

---
+++ src/main/java/stack/StackBlock.kt
@@ -0,0 +1,18 @@
+package stack
+
+import com.almasb.fxgl.entity.view.EntityView
+import javafx.scene.Node
+import javafx.scene.paint.Color
+import javafx.scene.shape.Rectangle
+
+class StackBlock(private val view: EntityView = EntityView()) {
+
+    fun get(): Node? {
+        return this.view
+    }
+
+    init {
+        view.addNode(Rectangle(20.0, 10.0, Color.BLUE))
+        view.addNode(Rectangle(10.0, 20.0, Color.RED))
+    }
+}
